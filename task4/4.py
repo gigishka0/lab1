a80481c1c0d92fc1d56d9f3a7f1fe9e4e5cb48d5
GiGi <pavlo.nesterenko1@nure.ua>
2025-12-05 15:35:22 +0200

    Add files via upload

---
+++ task4/4.py
@@ -0,0 +1,34 @@
+from telethon import TelegramClient
+from prettytable import PrettyTable
+
+api_id = 32300304
+api_hash = "c2aebbe8f1decc60095a6730feeea5ac"
+phone = "+380996506061"
+
+client = TelegramClient("lab1_session", api_id, api_hash)
+
+async def main():
+    await client.connect()
+    if not await client.is_user_authorized():
+        await client.send_code_request(phone)
+        code = input("Enter Telegram login code: ")
+        await client.sign_in(phone=phone, code=code)
+
+    chat_link = "https://t.me/+IaPpmAJ_sw8yZDVi"
+    chat = await client.get_entity(chat_link)
+    users = await client.get_participants(chat)
+
+    print(f"\n=== Group name: {chat.title} ===\n")
+
+    table = PrettyTable(["ID", "First name", "Last name"])
+
+    for user in users:
+        table.add_row([user.id, user.first_name, user.last_name])
+
+    print(table)
+
+    await client.send_message("@minonskyy", "test test test ")
+    print("\nMessage sent!")
+
+with client:
+    client.loop.run_until_complete(main())
